339f8cf7a4fb686939f47d8e5b83d02c015e0fa8
Keith-Njagi <keithgatana@gmail.com>
2020-01-15 18:58:34 +0300

    hosting ready

--- resources/__init__.py
+++ resources/__init__.py
@@ -6,4 +6,4 @@ from .todos import api as todos
 blueprint = Blueprint('api', __name__, url_prefix='/api')
 api = Api(blueprint, doc='/documentation', title='Todos API', version='1.0', description='An API to manage tasks')
 
-api.add_namespace(todos)
+api.add_namespace(todos, path='/todos')
