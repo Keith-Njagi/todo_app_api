506f4c280e21b8c2cc630321d26799033d39204c
Keith-Njagi <keithgatana@gmail.com>
2020-01-15 16:13:25 +0300

    hosting ready

--- app.py
+++ app.py
@@ -17,11 +17,13 @@ def create_app(config_class):
     db.init_app(app)
     ma.init_app(app)
     
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
+
     return app
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
 
 if __name__ == '__main__':
